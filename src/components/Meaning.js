f1cb733dbd50a238863f9fd2b75da70b9f0be861
Zahra F <zf77zf20@yahoo.com>
2022-11-30 16:06:27 +0330

    added synonyms

--- src/components/Meaning.js
+++ src/components/Meaning.js
@@ -4,19 +4,21 @@ export default function Meaning({ meaning }) {
       <h3 className="text-capitalize">{meaning.partOfSpeech}</h3>
       {meaning.definitions.map(function (definition, i) {
         return (
-          <div key={i}>
+          <div className="mb-3" align="justify" key={i}>
             <div>{definition.definition}</div>
-
             <div className="text-muted">{definition.example}</div>
-
-            <ul className="Synonyms">
-              {definition.synonyms.map(function (synonym, i) {
-                return <li key={i}>{synonym}</li>;
-              })}
-            </ul>
           </div>
         );
       })}
+      <ul className="Synonyms">
+        {meaning.synonyms.map(function (synonym, i) {
+          return (
+            <li className="btn btn-secondary m-1" key={i}>
+              {synonym}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
