4d84afb84a282263111321fb5bd5f8dc796361fa
Zahra F <zf77zf20@yahoo.com>
2022-11-30 13:13:47 +0330

    added fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./components/Dictionary";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Dictionary />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Dictionary.js
@@ -0,0 +1,30 @@
+import Search from "./Search";
+import Results from "./Results";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <div className="container">
+        <header>
+          <h1 className="text-center">Dictionary</h1>
+        </header>
+        <main>
+          <Search />
+          <Results />
+        </main>
+        <footer className="text-center">
+          <small className="text-muted">
+            Coded by Zahra F. Source code on{" "}
+            <a
+              href="https://github.com/zahraf1998/react-dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </small>
+        </footer>
+      </div>
+    </div>
+  );
+}
